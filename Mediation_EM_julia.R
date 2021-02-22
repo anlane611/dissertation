cf5416df3010362b5e789ef40898816bd36a457d
anlane611 <andrea.nicole.lane@emory.edu>
2021-02-22 15:56:50 -0500

    initial value analysis

--- Mediation_EM_julia.R
+++ Mediation_EM_julia.R
@@ -8,6 +8,8 @@ julia_library("Distributions")
 library(invgamma)
 
 ## initial values pulled from simulation code
+
+## mediating sites 5, 9; mediating cell types 1 and 4 respectively
 load("Nsample10000.RData")
 load("Nsample500.Rdata")
 
@@ -21,262 +23,48 @@ M <- M[i,]
 beta0 <- coefs[1:Ncell,i]
 beta1 <- coefs[(Ncell+1):(2*Ncell),i]
 pi <- trueProp
-# theta2 <- theta.2[i,]
 
 
-# theta0 <- theta0[i]
-# theta1 <- theta1[i]
-truem <- matrix(trueMethy[i,],Nsample,Ncell)
+#testing initial values
+theta0 <- 0
+theta1 <- 0
+
+
+tausq <- rep(rinvgamma(1,5,0.04),Ncell)
+sigmasq <- rinvgamma(1,5,0.04)
+gammasq <- (sd(O3)/5)^2
+
+
+
+theta2 <- c(0,0,0,0) #case 1
+site9cell1.lowvar.1 <- julia_call("IndefBoot",M, Y, E, beta0, beta1, pi, tausq,
+                                  sigmasq, gammasq, theta0, theta1, theta2)
+
+theta2 <- c(0.2,0.2,0.2,0.2) #case 2
+site9cell1.lowvar.2 <- julia_call("IndefBoot",M, Y, E, beta0, beta1, pi, tausq,
+                                  sigmasq, gammasq, theta0, theta1, theta2)
+
+theta2 <- c(0.3,0.1,0.1,0.1) #case 3
+site9cell1.lowvar.3 <- julia_call("IndefBoot",M, Y, E, beta0, beta1, pi, tausq,
+                                  sigmasq, gammasq, theta0, theta1, theta2)
+
+theta2 <- c(0.4,0,0,0) #case 4
+site9cell1.lowvar.4 <- julia_call("IndefBoot",M, Y, E, beta0, beta1, pi, tausq,
+                                  sigmasq, gammasq, theta0, theta1, theta2)
+
+site9cell1.lowvar <- rbind(site9cell1.lowvar.1,site9cell1.lowvar.2,site9cell1.lowvar.3,site9cell1.lowvar.4)
+
 
 #true values
 theta0 <- 0
 theta1 <- 0
+
 theta2 <- c(0,0,0,0.4)
 
 tausq <- rep((0.1)^2,Ncell)
 sigmasq <- 0.1^2
 gammasq <- (sd(O3)/5)^2
 
-## use true values as initial values
-res <- CTMEM(M=M, Y=Y, E=E, beta0=beta0, beta1=beta1, pi=pi, tausq=tausq,
-             sigmasq=sigmasq, gammasq=gammasq, theta0=theta0, theta1=theta1,
-             theta2=theta2, max.iter=200, tol=0.001)
-
-
-
-## function to calculate observed likelihood
-calc.lobs <- function(M, Y, E, beta0, beta1, pi, tausq,
-                    sigmasq, gammasq, theta0, theta1,
-                    theta2){
-
-  Nsample <- length(Y)
-  Ncell <- ncol(pi)
-  E.mat <- matrix(E,nrow=Nsample,ncol=Ncell)
-
-  ## initialize observed log-likelihood
-  beta1E <- sweep(E.mat,2,beta1,FUN="*") #beta1^kE
-  beta0beta1E <- sweep(beta1E,2,beta0,FUN="+") #beta0^k + beta1^kE
-  theta2beta <- sweep(beta0beta1E,2,theta2,FUN="*") #theta2^k + (beta0^k + beta1^kE)
-  
-  Y.mean <- theta0 + theta1*E + rowSums(theta2beta) #sum_k to get mean of Y
-  Y.var <- sum(theta2^2*tausq)+gammasq #sum_k theta2^2(k)*tau_k^2 + gamma^2
-  
-  M.mean <- rowSums(beta0beta1E*pi) #sum_k pi_k*(beta0^k+beta1^kE)
-  M.var <- rowSums(sweep(pi^2,2,tausq,FUN="*")) + sigmasq #sum_k pi_k^2*tau_k^2 + sigma^2
-  
-  l.obs <- 0
-  for(i in 1:Nsample){
-    y.part <- log(dnorm(Y[i],Y.mean[i],sqrt(Y.var)))
-    m.part <- log(dnorm(M[i],M.mean[i],sqrt(M.var[i])))
-    l.obs <- l.obs + y.part + m.part
-  }
-
-  return(l.obs)
-
-}
-
-
-#input initial values (from TCA) for now
-CTMEM <- function(M, Y, E, beta0, beta1, pi, tausq,
-                  sigmasq, gammasq, theta0, theta1,
-                  theta2, max.iter=1000, tol=0.001){
-  err <- 1000
-  iter <- 1
-  Nsample <- length(Y)
-  Ncell <- ncol(pi)
-  E.mat <- matrix(E,nrow=Nsample,ncol=Ncell)
-  
-  ## initialize observed log-likelihood
-  l.obs <- calc.lobs(M=M, Y=Y, E=E, beta0=beta0, beta1=beta1, pi=pi, tausq=tausq,
-                     sigmasq=sigmasq, gammasq=gammasq, theta0=theta0, theta1=theta1,
-                     theta2=theta2)
-  
-  ## initialize some temporary variables. Do this outside the EM loop
-  sigma.vec <- gamma.vec <- numeric(Nsample)
-  Sigmastar <- matrix(0, Ncell, Ncell*Nsample)
-  mustar.sig <- matrix(0,Nsample,Ncell)
-  mustar <- matrix(0,Nsample,Ncell)
-  theta2.denom <- vector("list", Nsample)
-  tau.vec <- matrix(0,Nsample,Ncell)
-  
-  while(err>=tol & iter<=max.iter){
-    ## E step: calculate mu* and sigma* (mean and variance of conditional distribution of m_k)
-    
-    Sigmastarinv.term1 <- (theta2%*%t(theta2))/gammasq
-    Sigmastarinv.term3 <- solve(diag(tausq))
-    for(i in 1:Nsample){
-      Sigmastarinv.term2 <- (pi[i,]%*%t(pi[i,]))/sigmasq
-      Sigmastar[,(Ncell*i-Ncell+1):(Ncell*i)] <- solve(Sigmastarinv.term1+
-                                                         Sigmastarinv.term2+Sigmastarinv.term3)
-    }
-    
-    for(i in 1:Nsample){
-      term1 <- ((Y[i]-theta0-theta1*E[i])*t(theta2))/gammasq
-      term2 <- (M[i]*t(pi[i,]))/sigmasq
-      term3 <- t(beta0+beta1*E[i])%*%solve(diag(tausq))
-      mustar.sig[i,] <- term1 + term2 + term3
-    }
-    
-    for(i in 1:Nsample){
-      mustar[i,] <- mustar.sig[i,]%*%Sigmastar[,(Ncell*i-Ncell+1):(Ncell*i)]
-    }
-    
-    
-    ## M-step
-    
-    # update beta0
-    beta1E <- sweep(E.mat,2,beta1,FUN = "*")
-    beta0.new <- (colSums(mustar-beta1E))/Nsample
-    
-    # update beta1
-    beta0mat <- matrix(beta0.new,nrow=Nsample,ncol=Ncell,byrow = TRUE)
-    beta1.new <- (colSums((mustar-beta0mat)*E.mat))/colSums(E.mat^2)
-    
-    # update theta0
-    mu.theta2 <- sweep(mustar,2,theta2,FUN="*")
-    theta0.new <- sum(Y-theta1*E-rowSums(mu.theta2))/Nsample
-    
-    # update theta1
-    theta1.new <- sum((Y-theta0.new-rowSums(mu.theta2))*E)/sum(E^2)
-    
-    # update theta2 (k-vector)
-    for(i in 1:Nsample){ #calculate denominator for each i
-      theta2.denom[[i]] <- Sigmastar[,(Ncell*i-Ncell+1):(Ncell*i)]+
-        (mustar[i,]%*%t(mustar[i,]))
-    }
-    denom <- solve(Reduce("+",theta2.denom)) #sum over i and take inverse
-    numer <- sweep(mustar,1,Y-theta0.new-theta1.new*E,FUN="*") #numerator: (Y-theta0-theta1E)mu*
-    theta2.new <- colSums(numer)%*%denom #sum numerator over i and multiply by denominator
-    theta2.new <- t(theta2.new)
-    
-    # update sigma^2
-    for(i in 1:Nsample){
-      sigma.vec[i] <- t(pi[i,])%*%Sigmastar[,(Ncell*i-Ncell+1):(Ncell*i)]%*%pi[i,] +
-        (M[i] - t(mustar[i,])%*%pi[i,])^2
-    }
-    sigmasq.new <- sum(sigma.vec)/Nsample
-    
-    # update gamma^2
-    for(i in 1:Nsample){
-      gamma.vec[i] <- t(theta2.new)%*%Sigmastar[,(Ncell*i-Ncell+1):(Ncell*i)]%*%theta2.new+
-        (Y[i] - theta0.new - theta1.new*E[i] - t(mustar[i,])%*%theta2.new)^2
-    }
-    gammasq.new <- sum(gamma.vec)/Nsample
-    
-    # update tau^2 (k-vector)
-    for(i in 1:Nsample){
-      for(j in 1:Ncell){
-        tau.vec[i,j] <- (mustar[i,j] - beta0.new[j] - beta1.new[j]*E[i])^2+
-          Sigmastar[,(Ncell*i-Ncell+1):(Ncell*i)][j,j]
-      }
-    }
-    tausq.new <- colSums(tau.vec)/Nsample
-    
-    ## evaluate observed log-likelihood to determine convergence
-    l.obs.new <- calc.lobs(M=M, Y=Y, E=E, beta0=beta0.new, beta1=beta1.new, pi=pi,
-                           tausq=tausq.new, sigmasq=sigmasq.new, gammasq=gammasq.new,
-                           theta0=theta0.new, theta1=theta1.new, theta2=theta2.new)
-    
-    err <- abs(l.obs.new - l.obs)
-    
-    # err2 <- sum(beta0.new-beta0)^2 + sum(beta1.new-beta0)^2 + (theta0.new-theta0)^2 +
-    #         (theta1.new-theta1)^2 + sum(theta2.new-theta2)^2 + (gammasq.new-gammasq)^2 +
-    #         (sigmasq.new-sigmasq)^2 + sum(tausq.new-tausq)^2
-    
-    #cat("iter", iter, ": llik=", l.obs.new, ", err=", err, "\n")
-    
-    #set new params
-    beta0 <- beta0.new
-    beta1 <- beta1.new
-    theta0 <- theta0.new
-    theta1 <- theta1.new
-    theta2 <- theta2.new
-    tausq <- tausq.new
-    sigmasq <- sigmasq.new
-    gammasq <- gammasq.new
-    
-    l.obs <- l.obs.new
-    
-    iter <- iter+1
-  }
-  
-  return(list(sigmasq=sigmasq, gammasq=gammasq, tausq=tausq,
-              beta0=beta0, beta1=beta1,
-              theta0=theta0, theta1=theta1, theta2=theta2,
-              err=err, mustar=mustar, iter=iter))
-  
-}
-
-## use true values as initial values
-time0 <- Sys.time()
-res <- CTMEM(M=M, Y=Y, E=E, beta0=beta0, beta1=beta1, pi=pi, tausq=tausq,
-      sigmasq=sigmasq, gammasq=gammasq, theta0=theta0, theta1=theta1,
-      theta2=theta2, max.iter=200, tol=0.01)
-diff2 <- Sys.time()-time0
-
-
-## use some random initial values
-Ncell <- ncol(pi)
-beta0.0 = rep(0.2, Ncell)
-beta1.0 = rep(0, Ncell)
-res = CTMEM(M=M, Y=Y, E=E, beta0=beta0.0, beta1=beta1.0, pi=pi, tausq=tausq,
-        sigmasq=sigmasq, gammasq=gammasq, theta0=theta0, theta1=theta1,
-        theta2=theta2, max.iter=1000, tol=0.01)
-
-
-### bootstrap for indirect effect
-
-IndefBoot <- function(M, Y, E, beta0, beta1, pi, tausq,
-                        sigmasq, gammasq, theta0, theta1,
-                        theta2, theIndex, b=100){
-    Ncell <- dim(pi)[2]
-    Nsample <- dim(pi)[1]
-    indef.b <- matrix(0,b,Ncell) # indirect effects for bootstrap
-    Indef.Sig <- c()
-    
-
-    EMout <- CTMEM(M=M, Y=Y, E=E, beta0=beta0, beta1=beta1, pi=pi, tausq=tausq,
-                   sigmasq=sigmasq, gammasq=gammasq, theta0=theta0, theta1=theta1,
-                   theta2=theta2, max.iter=500, tol=0.001)
-    obs.indef <- EMout$beta1*EMout$theta2
-    
-    for(nboot in 1:b){
-        print(nboot)
-        # get indices for bootstrap sample
-        #index.b <- sample(1:Nsample,Nsample,replace = TRUE)
-        index.b <- theIndex
-        E.b <- E[index.b]
-        M.b <- M[index.b]
-        pi.b <- pi[index.b,]
-        Y.b <- Y[index.b]
-
-        EMout.b <- CTMEM(M=M.b, Y=Y.b, E=E.b, beta0=beta0, beta1=beta1, pi=pi.b, tausq=tausq,
-                 sigmasq=sigmasq, gammasq=gammasq, theta0=theta0, theta1=theta1,
-                 theta2=theta2, max.iter=500, tol=0.001)
-
-  
-        indef.b[nboot,] <- EMout.b$beta1*EMout.b$theta2
-
-    } # end b
-
-    bootint <- apply(indef.b,2,function(x) quantile(x, c(.05/4,1-(.05/4))))
-
-    ## indirect effect in bootstrap interval?
-    for(i in 1:Ncell){
-        Indef.Sig[i] <- ifelse(0 < bootint[2,i] &
-                          0 > bootint[1,i],0,1)
-    }
-        
-        return(list(Indef.Sig=Indef.Sig, indirect.effect=obs.indef, bootint=bootint, 
-                    indef.b=indef.b))
-
-}
-
-time0 <- Sys.time()
-IndefBoot(M=M, Y=Y, E=E, beta0=beta0, beta1=beta1, pi=pi, tausq=tausq,
-                      sigmasq=sigmasq, gammasq=gammasq, theta0=theta0, theta1=theta1,
-                      theta2=theta2, theIndex=theIndex, b=2)
-diff2 <- Sys.time() - time0
 
 
 ### Julia ###
@@ -381,7 +169,7 @@ julia_command("
                 Nsample = length(Y)
                 Ncell = size(prop)[2]
                 Emat = repeat(E,1,4)
-                lobsvec = Array{Float64}(undef,maxiter,5)
+                lobsvec = Array{Float64}(undef,maxiter,6)
                 
                           #= ========================== =#
                           #= Define likelihood Function =#
@@ -418,7 +206,7 @@ julia_command("
                        
             
                 while iter<=maxiter && err>=tol
-                
+
                     #= ======= =#
                     #= E step  =#
                     #= ======= =#
@@ -488,6 +276,7 @@ julia_command("
                 
                 lobsvec[iter,1] = lobsnew
                 lobsvec[iter,2:5] = theta2new
+                lobsvec[iter,6] = iter
                 
                 #Core.println(lobsnew)
                 
@@ -576,7 +365,7 @@ julia_command("
         # 
         # return indefSig, ObsIE, mybootint
       
-        mylobsvec = EMout[10]
+        mylobsvec = EMout[5]
 
         return mylobsvec
 end")
@@ -613,95 +402,3 @@ julia_command("
         
 end")
 
-
-bootint <- apply(indef.b,2,function(x) quantile(x, c(.05/4,1-(.05/4))))
-
-time0 <- Sys.time()
-test0.45 <- julia_call("IndefBoot",M, Y, E, beta0, beta1, pi, tausq,
-           sigmasq, gammasq, theta0, theta1, theta2)
-diff <- Sys.time()-time0
-
-
-time0 <- Sys.time()
-test <- julia_call("EMBoot",Y.raw[c(5,9),], O, E, coefs[,c(5,9)], Prop)
-diff <- Sys.time()-time0
-
-time0 <- Sys.time()
-test <- julia_call("EMBoot",M, Y, E, coefs, pi)
-diff <- Sys.time()-time0
-
-
-theta1.new <- sum((Y-theta0.new-rowSums(mu.theta2))*E)/sum(E^2)
-
-#testing initial values
-beta0 <- c(0,0,0,0)
-beta1 <- c(0,0,0,0)
-theta0 <- 0
-theta1 <- 0
-theta2 <- c(-0.1,-0.1,-0.1,0.45)
-
-tausq <- rep(rinvgamma(1,5,0.04),Ncell)
-sigmasq <- rinvgamma(1,5,0.04)
-gammasq <- (sd(O3)/5)^2
-
-truevals <- c(0,0,0,0.4)
-par(mfrow=c(2,2))
-for(i in 2:4){
-  plot(test[,i],main=paste0("Cell ",i-1),xlab="Iteration",ylab="Estimate",ylim=c(-0.4,0.3))
-  abline(h=truevals[i-1],col="red")
-}
-plot(test[,5],main="Cell 4 (mediator)",xlab="Iteration",ylab="Estimate",ylim=c(0.1,0.5))
-abline(h=0.4,col="red")
-mtext("true values as initial values; max.iter=500, tol=0.01", side = 3, line = -1, outer = TRUE)
-
-
-par(mfrow=c(2,2))
-for(i in 2:4){
-  plot(test3[,i],main=paste0("Cell ",i-1),xlab="Iteration",ylab="Estimate",ylim=c(-0.4,0.3))
-  abline(h=truevals[i-1],col="red")
-}
-plot(test3[,5],main="Cell 4 (mediator)",xlab="Iteration",ylab="Estimate",ylim=c(0.1,0.5))
-abline(h=0.4,col="red")
-mtext("0 as initial values; max.iter=500, tol=0.01", side = 3, line = -1, outer = TRUE)
-
-
-truevals <- c(0,0,0,0.4)
-par(mfrow=c(2,2))
-for(i in 2:4){
-  plot(test2[,i],main=paste0("Cell ",i-1),xlab="Iteration",ylab="Estimate",ylim=c(-0.4,0.3))
-  abline(h=truevals[i-1],col="red")
-}
-plot(test2[,5],main="Cell 4 (mediator)",xlab="Iteration",ylab="Estimate",ylim=c(0.1,0.5))
-abline(h=0.4,col="red")
-mtext("true values as initial values; max.iter=500, tol=0.0001", side = 3, line = -1, outer = TRUE)
-
-
-par(mfrow=c(2,2))
-for(i in 2:4){
-  plot(test4[,i],main=paste0("Cell ",i-1),xlab="Iteration",ylab="Estimate",ylim=c(-0.4,0.3))
-  abline(h=truevals[i-1],col="red")
-}
-plot(test4[,5],main="Cell 4 (mediator)",xlab="Iteration",ylab="Estimate",ylim=c(0.1,0.5))
-abline(h=0.4,col="red")
-mtext("0 as initial values; max.iter=500, tol=0.0001", side = 3, line = -1, outer = TRUE)
-
-
-truevals <- c(0,0,0,0.4)
-par(mfrow=c(2,2))
-for(i in 2:4){
-  plot(test5[,i],main=paste0("Cell ",i-1),xlab="Iteration",ylab="Estimate",ylim=c(-0.4,0.3))
-  abline(h=truevals[i-1],col="red")
-}
-plot(test5[,5],main="Cell 4 (mediator)",xlab="Iteration",ylab="Estimate",ylim=c(0.1,0.5))
-abline(h=0.4,col="red")
-mtext("true values as initial values; max.iter=500, tol=0.0001", side = 3, line = -1, outer = TRUE)
-
-
-par(mfrow=c(2,2))
-for(i in 2:4){
-  plot(test6[,i],main=paste0("Cell ",i-1),xlab="Iteration",ylab="Estimate",ylim=c(-0.4,0.3))
-  abline(h=truevals[i-1],col="red")
-}
-plot(test6[,5],main="Cell 4 (mediator)",xlab="Iteration",ylab="Estimate",ylim=c(0.1,0.5))
-abline(h=0.4,col="red")
-mtext("0 as initial values; max.iter=2000, tol=0.0001", side = 3, line = -1, outer = TRUE)
